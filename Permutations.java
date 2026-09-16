788139d554187df547b449e75296df2668f45126
ajithaaa06 <harshithakb06@gmail.com>
2026-09-16 21:08:12 +0530

    Praticed on 16/09/26

---
+++ Permutations.java
@@ -0,0 +1,29 @@
+public class Permutations {
+
+    public static void findPermutation(String str, String ans) {
+
+        // Base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        // Choose every character
+        for (int i = 0; i < str.length(); i++) {
+
+            char curr = str.charAt(i);
+
+            String newStr = str.substring(0, i)
+                    + str.substring(i + 1);
+
+            findPermutation(newStr, ans + curr);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String str = "abc";
+
+        findPermutation(str, "");
+    }
+}
