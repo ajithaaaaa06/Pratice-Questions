659011743caf97d2f7979901c3c7199b363b79e4
ajithaaa06 <harshithakb06@gmail.com>
2026-09-18 15:03:24 +0530

    Praticed on 18/09/26

---
+++ t2X2queen.java
@@ -0,0 +1,37 @@
+public class t2X2queen {
+    public static void nQueens(char board[][],int row){
+        // base
+        if(row==board.length){
+            printBoard(board);
+            return;
+
+        }
+        //coloumn loop
+        for(int j=0;j<board.length;j++){
+            board[row][j]='Q';
+            nQueens(board,row+1); //function call
+            board[row][j]='.'; // backtrackign step
+        }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("-------Chess board----");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int n=2;
+        char board[][]=new char[n][n];
+        //initialize
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        nQueens(board,0);
+    }
+    
+}
