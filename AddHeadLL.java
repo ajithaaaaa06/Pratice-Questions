24e520b80b057750727a2746cfc018e9dd3b4969
ajithaaa06 <harshithakb06@gmail.com>
2026-09-26 00:03:04 +0530

    Praticed on 26/09/26

---
+++ AddHeadLL.java
@@ -0,0 +1,48 @@
+public class AddHeadLL {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+        AddHeadLL ll = new AddHeadLL();
+
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addFirst(0);
+
+        ll.print();
+    }
+}
